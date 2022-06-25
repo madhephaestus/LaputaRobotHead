04c77b9e668467d5cd73267ce16d7477cf1e363d
Kevin Harrington <mad.hephaestus@gmail.com>
2022-06-25 14:43:31 -0400

    updated

--- testVitamins.groovy
+++ testVitamins.groovy
@@ -1,1 +1,2 @@
-//Your code here
+CSG vitamin_capScrew_Inch_1_4_20 = Vitamins.get("capScrew", "Inch-1_4-20")
+CSG vitamin_ballBearing_Thrust_1andAHalfinch = Vitamins.get("ballBearing", "Thrust_1andAHalfinch")
